c05e23a932e5c7194860f5acd68f6d438707da85
ahmedhabeeb73 <alnaib888@gmail.com>
2025-04-25 22:17:17 +0200

    first

---
+++ src/main/java/com/ahmad/storee/StoreeApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.storee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmad/storee/StoreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.storee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
